c03f5a79037749c6cc0a361f20d4b3b66a25b5ea
Aman Hordofo <getamano@gmail.com>
2022-10-28 22:08:46 -0500

    Add connection to cosmosdb
    * get all todos
    * get a single to do item with id

--- TodoApp.cs
+++ TodoApp.cs
@@ -6,30 +6,100 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+
+using Microsoft.Azure.Cosmos;
+using System.Collections.Generic;
+
 
 namespace Imronet.Function
 {
     public static class TodoApp
     {
-        [FunctionName("TodoApp")]
+        [FunctionName("GetTodos")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            string connectionString = Environment.GetEnvironmentVariable("MyConnectionString", EnvironmentVariableTarget.Process);
 
-            string name = req.Query["name"];
+            // New instance of CosmosClient class using a connection string
+            using CosmosClient _client = new(
+                connectionString: connectionString
+            );
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            Container _container = _client.GetDatabase("TestDB").GetContainer("todos");
+            
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            List<Todo> todoDataArray = new List<Todo>();
 
-            return new OkObjectResult(responseMessage);
+            // Build query definition
+            var parameterizedQuery = new QueryDefinition(
+                query: "SELECT * FROM todos c"
+            );
+               
+
+            // Query multiple items from container
+            using FeedIterator<Todo> todoData = _container.GetItemQueryIterator<Todo>(
+                queryDefinition: parameterizedQuery
+            );
+
+            // Iterate query result pages
+            while (todoData.HasMoreResults)
+            {
+                FeedResponse<Todo> todos = await todoData.ReadNextAsync();
+
+                // Iterate query results
+                foreach (Todo todo in todos)
+                {
+                    todoDataArray.Add(todo);
+                }
+            }
+
+            return new OkObjectResult(todoDataArray);
         }
+
+        [FunctionName("GetTodo")]
+        public static async Task<IActionResult> Task(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            string connectionString = Environment.GetEnvironmentVariable("MyConnectionString", EnvironmentVariableTarget.Process);                   
+                   
+             // New instance of CosmosClient class using a connection string
+            using CosmosClient _client = new(
+                connectionString: connectionString
+            );
+
+            Container _container = _client.GetDatabase("TestDB").GetContainer("todos");
+
+            string id = req.Query["id"]; // get todo id from the url query 
+
+
+            // Build query definition
+            var parameterizedQuery = new QueryDefinition(
+                query: "SELECT * FROM todos c WHERE c.id = @id"
+            ).WithParameter("@id", id);
+
+
+            // Query multiple items from container
+            using FeedIterator<Todo> todoData = _container.GetItemQueryIterator<Todo>(
+                queryDefinition: parameterizedQuery
+            );
+
+            FeedResponse<Todo> todo = await todoData.ReadNextAsync();
+        
+            return new OkObjectResult(todo);
+        }
+    }
+
+
+    public class Todo {
+        public String id {get; set;}
+        public String title  {get; set;}
+        public String description  {get; set;}
+
+        public Boolean isCompleted  {get; set;}
+
     }
 }
